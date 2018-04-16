42c6fe81099ea38eac345b8f2bd859e9ad250a72
Anthony Odufuye <odufuye.t@gmail.com>
2018-04-16 17:40:24 -0500

    added retries with failsafe

--- search/src/main/java/SearchCaseStudy.java
+++ search/src/main/java/SearchCaseStudy.java
@@ -3,14 +3,19 @@ import com.google.common.io.Files;
 import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import net.jodah.failsafe.Failsafe;
+import net.jodah.failsafe.RetryPolicy;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class SearchCaseStudy {
@@ -24,8 +29,9 @@ public class SearchCaseStudy {
 
         try {
             final OptionSet optionSet = optionParser.parse(args);
+            final String directory = optionSet.valueOf("directory").toString();
             Iterator<File> fileIterator = Files.fileTraverser()
-                    .breadthFirst(new File(optionSet.valueOf("directory").toString()))
+                    .breadthFirst(new File(directory))
                     .iterator();
 
             // Todo: Explain how consuming the iterator can affect performance with large quantities of files, and how it can reach the limit on file handles
@@ -34,15 +40,43 @@ public class SearchCaseStudy {
                     .filter(File::isFile)
                     .filter(x -> x.getName().endsWith(".txt"))
                     .collect(Collectors.toList());
-            System.out.println(String.format("The directory is: %s and it contains files: %s",
-                    optionSet.valueOf("directory"),
-                    files));
+
+            if (!files.isEmpty()) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                    System.out.print("Enter the search term: ");
+                    String input = br.readLine();
+
+
+                    RetryPolicy retryPolicy = new RetryPolicy()
+                            .retryOn(NumberFormatException.class)
+                            .withDelay(1, TimeUnit.SECONDS)
+                            .withMaxRetries(3);
+
+                    int method = Failsafe.with(retryPolicy)
+                            .onFailedAttempt(failure -> System.out.println(String.format("Error %s. Please try again",
+                                    failure)))
+                            .get(() -> {
+                                System.out.print("Search Method: 1) String Match, 2) Regular Expression, 3) Indexed ");
+                                return Integer.parseInt(br.readLine());
+                            });
+
+                    System.out.println(String.format("Search term: %s, Method: %s", input, method));
+
+                } catch (IOException io) {
+                    logger.error("Error occurred while reading input from the console", io);
+                } catch (NumberFormatException ne) {
+                    logger.error("\nEnding document search. Please try again with a valid Search Method");
+                }
+            } else {
+                logger.info(String.format("There are no text files in the directory %s", directory));
+            }
+
 
         } catch (OptionException oe) {
             try {
                 optionParser.printHelpOn(System.out);
             } catch (IOException io) {
-                logger.info("Exception occured while attempting to print to system out", io);
+                logger.error("Exception occured while attempting to print to system out", io);
             }
         }
 
