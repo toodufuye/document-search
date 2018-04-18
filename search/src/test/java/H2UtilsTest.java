07762e383f066451a91861d49add6e36cec5aa9a
Anthony Odufuye <toodufuye@spscommerce.com>
2018-04-18 10:58:41 -0500

    checking in the H2Utils test

---
+++ search/src/test/java/H2UtilsTest.java
@@ -0,0 +1,23 @@
+import io.vavr.collection.List;
+import org.jdbi.v3.core.Jdbi;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.mockito.Mockito.*;
+
+public class H2UtilsTest {
+    @Test
+    public void createInstance() {
+        new H2Utils();
+        // Fun fact, static classes with no instances show up on coverage reports as a missing branch.
+    }
+
+    @Test
+    public void insertWordsIntoDatabase() {
+        File testFile = mock(File.class);
+        Jdbi mockJdbi = mock(Jdbi.class);
+        H2Utils.insertWordsIntoDatabase(mockJdbi, List.of(testFile));
+        verify(mockJdbi).useExtension(any(), any());
+    }
+}
